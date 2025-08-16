30a200a189c8363481b42c345cb2eb118afe29f2
prakashkambli <howtocreate2021@gmail.com>
2025-08-16 15:35:39 +0530

    Merge branch 'feature/sqrt' into dev

--- calculator plus.py
+++ calculator plus.py
@@ -1,7 +1,6 @@
 import math
 
 class Calculator:
-
     def add(self, a, b):
         return a + b
 
@@ -12,9 +11,10 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
-        if b == 0:
-             raise ValueError("Cannot divide by zero.")
-        return a / b
+ Temporary merge branch 1
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
 
     def square_root(self, x):
         if x < 0:
@@ -26,11 +26,26 @@ if __name__ == "__main__":
 
     num1 = 16
     num2 = 4
+=========
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
+
+
+# ----------------- Test -----------------
+if __name__ == "__main__":
+    calculator = Calculator()
+
+    num1, num2 = 16, 4
+>>>>>>>>> Temporary merge branch 2
 
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+<<<<<<<<< Temporary merge branch 1
 
     num3 = 25
     print(f"The square root of {num3} = {calculator.square_root(num3)}")
+=========
+>>>>>>>>> Temporary merge branch 2

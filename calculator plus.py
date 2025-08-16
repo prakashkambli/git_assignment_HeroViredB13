5f847978e74e9804a71f858161c43aa80caa7050
prakashkambli <howtocreate2021@gmail.com>
2025-08-16 14:25:02 +0530

    FIx Divide function in calculator

---
+++ calculator plus.py
@@ -0,0 +1,28 @@
+import math
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
+
+
+# ----------------- Test -----------------
+if __name__ == "__main__":
+    calculator = Calculator()
+
+    num1, num2 = 16, 4
+
+    print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
+    print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
+    print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+    print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
